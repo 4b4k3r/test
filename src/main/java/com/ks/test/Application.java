938d8e5a7cd8c508ddd437f1e853ddff81f65d9d
Joel Medina <jmedina@kssoluciones.com>
2021-04-13 18:00:23 -0500

    1.2 - Se agrega mensaje para desarrollo

--- src/main/java/com/ks/test/Application.java
+++ src/main/java/com/ks/test/Application.java
@@ -4,6 +4,6 @@ public class Application
 {
     public static void main(String[] args)
     {
-        System.out.println("Proyecto inicial de joel");
+        System.out.println("Proyecto inicial de joel en desarrollo");
     }
 }
